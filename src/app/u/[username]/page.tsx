bd5696acad62ba06f7dd506593a4d79b68cd9650
kuntal-hrithik <ritikkuntalkhari@gmail.com>
2024-10-25 19:02:48 +0530

    added profile route

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,8 +1,96 @@
 "use client";
-import React from "react";
-
+import React, { useState } from "react";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import { toast } from "@/hooks/use-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { messageSchema } from "@/schemas/messageSchema";
+import { useParams } from "next/navigation";
+import { Loader2 } from "lucide-react";
 const ProfilePage = () => {
-  return <div>Message Page</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const params = useParams<{ username: string }>();
+  const username = params.username;
+
+  const form = useForm<z.infer<typeof messageSchema>>({
+    resolver: zodResolver(messageSchema),
+  });
+
+  const onSubmit = async (data: z.infer<typeof messageSchema>) => {
+    try {
+      setIsLoading(true);
+      await axios.post("/api/send-message", {
+        ...data,
+        username,
+      });
+      form.reset({ ...form.getValues(), content: "" });
+      toast({
+        title: "Success",
+        description: "Message sent successfully",
+      });
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title: "Error",
+        description: axiosError.response?.data.message,
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="container mx-auto my-8 p-6 bg-white rounded max-w-4xl">
+      <h1 className="text-4xl font-bold mb-6 text-center">
+        Public Profile Link
+      </h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Send Anonymous Message to @{username}</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Write your anonymous message here"
+                    className="resize-none"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-center">
+            {isLoading ? (
+              <Button disabled>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+              </Button>
+            ) : (
+              <Button type="submit" disabled={isLoading}>
+                Send It
+              </Button>
+            )}
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default ProfilePage;
